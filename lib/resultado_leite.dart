a9e3f5ee97b085855e644cc06cbf5ca3dd9b2bc6
gabrielapinto <gabrielapinto@ufcspa.edu.br>
2023-03-17 14:27:45 -0300

    leituras e resultado leite

--- lib/resultado_leite.dart
+++ lib/resultado_leite.dart
@@ -1,4 +1,5 @@
 
+import 'package:appbovconfort/dados.dart';
 import 'package:appbovconfort/faixa2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -7,6 +8,7 @@ import 'package:appbovconfort/passos_br.dart';
 import 'package:appbovconfort/faixa3.dart';
 import 'package:appbovconfort/faixa4.dart';
 import 'package:appbovconfort/faixa5.dart';
+import 'package:appbovconfort/leituras_comp.dart';
 
 class Resultado_leite extends StatefulWidget {
   const Resultado_leite({super.key});
@@ -26,15 +28,14 @@ class _HomeState extends State<Resultado_leite> {
     var size = MediaQuery.of(context).size;
     var screenHeight = (size.height - appBar.preferredSize.height) -
         MediaQuery.of(context).padding.top;
-    final ButtonStyle botaoLeite = ElevatedButton.styleFrom(
-      foregroundColor: Colors.black,
-      backgroundColor: Colors.grey,
-      minimumSize: const Size(88, 36),
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-      ),
-    );
+    final ButtonStyle botaoCalcular = ElevatedButton.styleFrom(
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.grey,
+        minimumSize: const Size(88, 36),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ));
     return Scaffold(
         backgroundColor: Colors.teal[50],
         appBar: appBar,
@@ -43,81 +44,79 @@ class _HomeState extends State<Resultado_leite> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-               Container(
+              Container(
+                  //color: Colors.blue,
                   height: screenHeight * .1,
-                  width: size.width*.8,
-                  margin: EdgeInsets.only(bottom: screenHeight * .05),
+                  //margin: EdgeInsets.only(bottom: screenHeight * .03),
+                    child: Text("RESULTADO",
+                        style: TextStyle(
+                            fontSize: 20.sp,
+                            fontFamily: "OpenSans",
+                            fontWeight: FontWeight.w800)),
+                  ),
+              Container(
+                  //color: Colors.green,
+                  height: screenHeight * .1,
+                  margin: EdgeInsets.only(bottom: screenHeight * .01),
                   child: Center(
-                    child: Text("Seu resultado de perda estimada de leite é:",
+                    child: Text("O valor estimado \nde perda de leite é:",
+                        style: TextStyle(
+                        fontSize: 16.sp,
+                        fontFamily: "OpenSans",
+                        fontWeight: FontWeight.w600)),
+                        )
+              ),
+              SizedBox(height: size.height*.07),
+              Container(
+                  height: screenHeight * .1,
+                  width: size.width * .5,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: Color.fromARGB(255, 168, 229, 213),),
+                  //margin: EdgeInsets.only(bottom: screenHeight * .03),
+                  child: Center(
+                    child: Text(
+                        " 0.00",
+                        style: TextStyle(
+                            fontSize: 15.sp,
+                            fontFamily: "OpenSans",
+                            fontWeight: FontWeight.w400)),
+                  )),
+                  SizedBox(height: size.height*.02),
+              Container(child: Center(child: Text("Litros/Dia", style: TextStyle(fontSize: 12.sp)))),
+              SizedBox(height: size.height*.08),
+              Container(
+                  width: size.width * .6,
+                  height: screenHeight * .1,
+                  child: ElevatedButton(
+                    style: botaoCalcular,
+                    onPressed: () {
+                      Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const Dados()));
+                    },
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text('Como Atenuar o\nEstresse Térmico',
                         textAlign: TextAlign.center,
                         style: TextStyle(
-                            fontSize: 18.sp,
-                            fontFamily: "OpenSans",
-                            fontWeight: FontWeight.w800,
-                            )),
-                  )),
-              SizedBox(height: size.height*0.05),
-                  Column(children: <Widget>[
-                    Text("Máx Umidade Relativa",
-                        style: TextStyle(fontWeight: FontWeight.w600)),
-                    SizedBox(
-                      width: size.width*0.4,
-                      height: size.height*0.1,
-                      child: TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                          LengthLimitingTextInputFormatter(4),
-                        ],
-                        decoration: const InputDecoration(
-                            hintText: "0.00",
-                            border: OutlineInputBorder(),
-                            fillColor: Colors.white),
-                        style: TextStyle(fontSize: 18),
-                      ),
+                          fontSize: 12.sp,
+                          fontWeight: FontWeight.w800,
+                        )), // <-- Text
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(
+                          // <-- Icon
+                          Icons.question_mark_outlined,
+                          size: 30.0,
+                        ),
+                      ],
                     ),
-                  ]),
-                  SizedBox(width: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(width: size.width * .1),
-                  Container(
-                width: size.width * .6,
-                height: screenHeight * .1,
-                child: ElevatedButton(
-                  style: botaoLeite,
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const Resultado_leite()));
-                  },
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text('Calcular Perda \n Estimada de Leite',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w600,
-                          )), // <-- Text
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Icon(
-                        // <-- Icon
-                        Icons.arrow_forward,
-                        size: 24.0,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-                ],
-              ),
+                  ),),
               SizedBox(height: screenHeight * .01),
-    ])));
+            ])));
   }
 }
-
